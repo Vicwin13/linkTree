1fc9194f08b5292d9a7d649e922d6d3ad4bd39ea
vicwin13 <vnwimo13@gmail.com>
2022-10-27 12:06:43 +0100

    added the buttons

--- linktr/src/App.js
+++ linktr/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
+import Buttons from "./Buttons";
 import Profile from "./Profile";
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Buttons />
     </div>
   );
 }
---
+++ linktr/src/Buttons.js
@@ -0,0 +1,32 @@
+function Buttons() {
+  return (
+    <main>
+      <button>
+        {" "}
+        <a href="##"> Twitter</a>
+      </button>
+      <button>
+        {" "}
+        <a href="##"> Zuri Team</a>
+      </button>
+      <button>
+        {" "}
+        <a href="##"> Zuri Books</a>
+      </button>
+      <button>
+        {" "}
+        <a href="##"> Phyton Books</a>
+      </button>
+      <button>
+        {" "}
+        <a href="##"> Background check for codes</a>
+      </button>
+      <button>
+        {" "}
+        <a href="##"> Design Books</a>
+      </button>
+    </main>
+  );
+}
+
+export default Buttons;
