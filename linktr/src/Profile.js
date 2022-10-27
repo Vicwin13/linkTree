e60ea72fcbfbb7b4be9ac31d3bbaf9c4949fa449
vicwin13 <vnwimo13@gmail.com>
2022-10-27 11:53:04 +0100

    initial commit

---
+++ linktr/src/Profile.js
@@ -0,0 +1,13 @@
+const Profile = () => {
+  const fullName = "Victor Nwimo";
+  // const slack = "Winner";
+
+  return (
+    <header>
+      <div id="profile_img" className="images"></div>
+      <p>{fullName}</p>
+    </header>
+  );
+};
+
+export default Profile;
